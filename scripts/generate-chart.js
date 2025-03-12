f9f15b10497f33ea79944cdd7d7fc3ed913f5e74
Anderson Silva <148403926+DevHoxx@users.noreply.github.com>
2025-03-11 21:39:17 -0300

    Update generate-chart.js

--- scripts/generate-chart.js
+++ scripts/generate-chart.js
@@ -1,1 +1,8 @@
+const fs = require('fs');
+const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
 
+async function generateChart() {
+  // ... (código completo do Passo 2)
+}
+
+generateChart();
